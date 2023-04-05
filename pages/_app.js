2f3da88c229287f059280fa0c50d3443e0534ea7
ykdevelops <ykdevelops@gmail.com>
2023-04-05 01:12:56 -0400

    feat: :zap: Added motion animation to pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
     return (
         <CartProvider>
-            <Component {...pageProps} />
+            <AnimatePresence mode='wait'>
+                <motion.div
+                    key={Component.pageConfig?.key || Component.name}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, transition: { duration: 0.1 } }}
+                >
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
         </CartProvider>
     );
 }
