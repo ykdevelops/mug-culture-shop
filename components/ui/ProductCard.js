f1dddf7711fb07e9245635f20458c3d86316a663
ykdevelops <ykdevelops@gmail.com>
2023-04-14 18:45:03 -0400

    feat: :zap: Added Stripe buy now feature

--- components/ui/ProductCard.js
+++ components/ui/ProductCard.js
@@ -2,9 +2,21 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import productStyles from '../../styles/product.module.css';
 import { AnimatePresence, motion } from 'framer-motion';
+import stripeLinks from '../../data/stripeLinks';
+import { useCart } from '../../context/CartContext';
 
 const ProductCard = ({ product }) => {
-
+    const {
+        cart,
+        addToCart,
+        removeItem,
+        clearCart,
+        isCheckingOut,
+        proceedToCheckout,
+        handleCheckout,
+        total,
+        setCart,
+    } = useCart();
     const [isHovered, setIsHovered] = useState(false);
 
     const handleHover = () => {
@@ -13,6 +25,12 @@ const ProductCard = ({ product }) => {
 
     const handleMouseLeave = () => {
         setIsHovered(false);
+    };
+
+    const handleBuyNow = (e) => {
+        e.preventDefault(); // prevent the default behavior of the button
+        const stripeLink = stripeLinks.find(link => link.id === product.id);
+        window.location.href = stripeLink.link;
     };
 
     const productVariants = {
@@ -27,24 +45,58 @@ const ProductCard = ({ product }) => {
             boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)',
         },
     };
+    const handleAddToCart = (event) => {
+        event.stopPropagation();
+        const existingItemIndex = cart.findIndex(
+            (item) => item.product.id === product.id
+        );
+        if (existingItemIndex !== -1) {
+            const updatedCart = [...cart];
+            updatedCart[existingItemIndex].quantity += 1;
+            setCart(updatedCart);
+        } else {
+            addToCart(product, 1);
+        }
+
+        console.log('Cart:', cart);
+    };
+
 
     return (
-        <>
-            <Link href={`/${product.id}`}>
-                <motion.div
-                    className={productStyles.product}
-                    variants={productVariants}
+
+        <motion.div
+            className={productStyles.product}
+            variants={productVariants}
+            whileHover="hovered"
+            whileTap="normal"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleMouseLeave}
+        >
+            <Link href={`/${product.id}`} className={productStyles.linkBox}>
+                <img src={product.thumbnail} alt={product.name} />
+                <span className={productStyles.name}>{product.name}</span>
+                <span className={productStyles.price}>${product.price}</span>
+            </Link>
+
+            <div className={productStyles.buttonRow}>
+                <motion.button
                     whileHover="hovered"
                     whileTap="normal"
                     onMouseEnter={handleHover}
                     onMouseLeave={handleMouseLeave}
-                >
-                    <img src={product.thumbnail} alt={product.name} />
-                    <span className={productStyles.name}>{product.name}</span>
-                    <span className={productStyles.price}>${product.price}</span>
-                </motion.div>
-            </Link>
-        </>
+                    variants={productVariants}
+                    className={productStyles.button}
+                    onClick={handleBuyNow}>Buy Now</motion.button>
+                <motion.button whileHover="hovered"
+                    whileTap="normal"
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleMouseLeave}
+                    variants={productVariants}
+                    className={productStyles.button} onClick={handleAddToCart}>Add to Cart</motion.button>
+            </div>
+
+        </motion.div>
+
     );
 };
 
---
+++ data/stripeLinks.js
@@ -0,0 +1,40 @@
+const stripeLinks = [
+    {
+        id: 1,
+        link: 'https://buy.stripe.com/test_28o3f2aaAedJ9LqfYZ'
+    },
+    {
+        id: 2,
+        link: 'https://buy.stripe.com/test_00g9Dq4QgglR9LqdQS'
+    },
+    {
+        id: 3,
+        link: 'https://buy.stripe.com/test_eVa4j62I86Lh5va003'
+    },
+    {
+        id: 4,
+        link: 'https://buy.stripe.com/test_6oE2aY4Qgc5B9Lq5ko'
+    },
+    {
+        id: 5,
+        link: 'https://buy.stripe.com/test_bIYaHu4QgglRbTy9AF'
+    },
+    {
+        id: 6,
+        link: 'https://buy.stripe.com/test_cN2aHufuU7Plf5KfZ4'
+    },
+    {
+        id: 7,
+        link: 'https://buy.stripe.com/test_5kA16U3McedJ5va5kr'
+    },
+    {
+        id: 8,
+        link: 'https://buy.stripe.com/test_8wMg1OfuU1qX2iYaEM'
+    },
+    {
+        id: 9,
+        link: 'https://buy.stripe.com/test_8wMbLyaaA6Lh6ze9AJ'
+    },
+];
+
+export default stripeLinks;
